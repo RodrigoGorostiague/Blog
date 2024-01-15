f5642b2668bd482b8e594c041d41083c3e8d4128
rgorostiague <rgorostiague@sisptandil.gob.ar>
2024-01-15 11:42:56 -0300

    se comienza con la implementacion del dark-mode

--- src/app/domains/shared/components/navbar/navbar.component.ts
+++ src/app/domains/shared/components/navbar/navbar.component.ts
@@ -11,4 +11,52 @@ import { RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 })
 export class NavbarComponent {
 
+  themeToggleDarkIcon: HTMLElement | null = null;
+  themeToggleLightIcon: HTMLElement | null = null;
+  themeToggleBtn: HTMLElement | null = null;
+
+  ngOnInit(): void {
+    this.themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+    this.themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+    this.themeToggleBtn = document.getElementById('theme-toggle');
+
+    if (
+      localStorage.getItem('color-theme') === 'dark' ||
+      (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
+      this.themeToggleLightIcon?.classList.remove('hidden');
+    } else {
+      this.themeToggleDarkIcon?.classList.remove('hidden');
+    }
+
+    if (this.themeToggleBtn) {
+      this.themeToggleBtn.addEventListener('click', () => {
+        this.toggleTheme();
+      });
+    }
+  }
+
+  toggleTheme(): void {
+    if (localStorage.getItem('color-theme')) {
+      if (localStorage.getItem('color-theme') === 'light') {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+      } else {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+      }
+    } else {
+      if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem('color-theme', 'light');
+      } else {
+        document.documentElement.classList.add('dark');
+        localStorage.setItem('color-theme', 'dark');
+      }
+    }
+
+    this.themeToggleDarkIcon?.classList.toggle('hidden');
+    this.themeToggleLightIcon?.classList.toggle('hidden');
+  }
+
 }
